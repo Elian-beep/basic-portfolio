db57e61b366e2cb752261eb1477a90118ac510c4
Elian-beep <elianoliver101@gmail.com>
2022-04-11 10:26:30 -0300

    correcao da capa

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,94 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Actions, ActionsCard, AreaExposition, BottomCard, BottomText, Card, Container, Content, MediaCard, SecondText, Title, TopCard } from '../../../styles/pageProjects/style';
 
 export default function Projects() {
+
+    const [itemsApi, setItemsApi] = useState([]);
+
+    useEffect(() => {
+        let abortController = new AbortController();
+
+        function getGitHubAPI() {
+            fetch('https://api.github.com/users/Elian-beep/repos')
+                .then(async res => {
+                    if (!res.ok) {
+                        throw new Error(res.status);
+                    }
+
+                    var data = await res.json();
+                    setItemsApi(data);
+                })
+                .catch(e => console.log(e));
+        }
+
+        getGitHubAPI();
+
+        return () => abortController.abort();
+    }, []);
+
     return (
         <Container>
             <Content>
                 <AreaExposition>
-                    <Card>
+                    {itemsApi.map(item => (
+                        <Card key={item.id}>
+                            <TopCard>
+                                <Title>{item.name.toUpperCase()}</Title>
+                                <SecondText>Exemplo de texto secundário</SecondText>
+                            </TopCard>
 
-                        <TopCard>
-                            <Title>Titulo do Card</Title>
-                            <SecondText>Exemplo de texto secundário</SecondText>
-                        </TopCard>
+                            <MediaCard>
+                                <img src={"https://raw.githubusercontent.com/Elian-beep/"+ item.name+ "/main/capa/capa.png"} alt="capa_projeto" />
+                            </MediaCard>
 
-                        <MediaCard>
-                            <img src="/images/capa_projeto.jpg" alt="capa_projeto" />
-                        </MediaCard>
+                            <BottomCard>
+                                <BottomText>{item.description}</BottomText>
+                                <ActionsCard>
+                                    <Actions>Visitar Página</Actions>
+                                    <Actions>Ver no Github</Actions>
+                                </ActionsCard>
+                            </BottomCard>
+                        </Card>
+                    ))}
 
-                        <BottomCard>
-                            <BottomText>Aqui é um exemplo de resumo de página ou do documneto que o card trata</BottomText>
-                            <ActionsCard>
-                                <Actions>Visitar Página</Actions>
-                                <Actions>Ver no Github</Actions>
-                            </ActionsCard>
-                        </BottomCard>
-
-                    </Card>
-
-                    <Card>
-
-                        <TopCard>
-                            <Title>Titulo do Card</Title>
-                            <SecondText>Exemplo de texto secundário</SecondText>
-                        </TopCard>
-
-                        <MediaCard>
-                            <img src="/images/capa_projeto.jpg" alt="capa_projeto" />
-                        </MediaCard>
-
-                        <BottomCard>
-                            <BottomText>Aqui é um exemplo de resumo de página ou do documneto que o card trata</BottomText>
-                            <ActionsCard>
-                                <Actions>Visitar Página</Actions>
-                                <Actions>Ver no Github</Actions>
-                            </ActionsCard>
-                        </BottomCard>
-
-                    </Card>
-
-                    <Card>
-
-                        <TopCard>
-                            <Title>Titulo do Card</Title>
-                            <SecondText>Exemplo de texto secundário</SecondText>
-                        </TopCard>
-
-                        <MediaCard>
-                            <img src="/images/capa_projeto.jpg" alt="capa_projeto" />
-                        </MediaCard>
-
-                        <BottomCard>
-                            <BottomText>Aqui é um exemplo de resumo de página ou do documneto que o card trata</BottomText>
-                            <ActionsCard>
-                                <Actions>Visitar Página</Actions>
-                                <Actions>Ver no Github</Actions>
-                            </ActionsCard>
-                        </BottomCard>
-
-                    </Card>
-
-                    <Card>
-
-                        <TopCard>
-                            <Title>Titulo do Card</Title>
-                            <SecondText>Exemplo de texto secundário</SecondText>
-                        </TopCard>
-
-                        <MediaCard>
-                            <img src="/images/capa_projeto.jpg" alt="capa_projeto" />
-                        </MediaCard>
-
-                        <BottomCard>
-                            <BottomText>Aqui é um exemplo de resumo de página ou do documneto que o card trata</BottomText>
-                            <ActionsCard>
-                                <Actions>Visitar Página</Actions>
-                                <Actions>Ver no Github</Actions>
-                            </ActionsCard>
-                        </BottomCard>
-
-                    </Card>
                 </AreaExposition>
             </Content>
         </Container>
