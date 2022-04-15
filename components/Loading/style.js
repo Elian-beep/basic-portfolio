abc092f81fdb824b060ef3e7fe32e89b64ee71cc
Elian-beep <elianoliver101@gmail.com>
2022-04-15 12:29:19 -0300

    projects: correção final do loading mobile

--- components/Loading/style.js
+++ components/Loading/style.js
@@ -4,8 +4,17 @@ export const AreaLoading = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    margin: 0;
-    padding: 0;
-    border: 0;
     height: calc(100vh - 100px); //**Irá ocupar 100vh da tela ignorando os 100px do header
+`;
+
+export const ContentLoading = styled.div`
+    @media(max-width: 600px){ //**Se for menor que 600px
+        padding: 5px;
+        width: 1120px;
+        height: 100%;
+
+        justify-content: center;
+        display: flex;
+        align-items: center;
+    }
 `;
